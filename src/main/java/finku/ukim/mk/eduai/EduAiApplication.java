b41aec03553c452d58af065208bcb120337a29af
vercchy <verchepetrushevska2@gmail.com>
2025-04-26 16:19:36 +0200

    Initial commit with initial application properties

---
+++ src/main/java/finku/ukim/mk/eduai/EduAiApplication.java
@@ -0,0 +1,13 @@
+package finku.ukim.mk.eduai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finku/ukim/mk/eduai/EduAiApplicationTests.java
@@ -0,0 +1,13 @@
+package finku.ukim.mk.eduai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
