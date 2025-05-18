dca7c3ac74520911ee2389b35c7cdf73efb50a24
LorenBosh <lorenbosko@gmail.com>
2025-05-18 22:58:20 +0200

    fill tables

---
+++ database/V1__fill-tables.sql
@@ -0,0 +1,125 @@
+-- hash for Password123$
+INSERT INTO users (first_name, last_name, email, password, role)
+VALUES
+    ('Ana', 'Petrova', 'ana.petrova@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Marko', 'Ivanov', 'marko.ivanov@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Elena', 'Stojanovska', 'elena.stojanovska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Stefan', 'Kostov', 'stefan.kostov@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Marija', 'Georgievska', 'marija.georgievska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Nikola', 'Jovanov', 'nikola.jovanov@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Simona', 'Todorovska', 'simona.todorovska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Kristijan', 'Mitrevski', 'kristijan.mitrevski@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Jovana', 'Koleva', 'jovana.koleva@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Filip', 'Petreski', 'filip.petreski@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Ivana', 'Ilievska', 'ivana.ilievska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Martin', 'Gjorgjiev', 'martin.gjorgjiev@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Sara', 'Nikolovska', 'sara.nikolovska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Aleksandar', 'Velkov', 'aleksandar.velkov@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Milena', 'Ristovska', 'milena.ristovska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Goran', 'Angelov', 'goran.angelov@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Teodora', 'Krstevska', 'teodora.krstevska@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('David', 'Spasevski', 'david.spasevski@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Kristina', 'Dimitrova', 'kristina.dimitrova@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+    ('Viktor', 'Naumov', 'viktor.naumov@students.finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'STUDENT'),
+
+    --PROFESSOR
+    ('Dragan', 'Petrov', 'dragan.petrov@finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'PROFESSOR'),
+    ('Maja', 'Stankovska', 'maja.stankovska@finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'PROFESSOR'),
+    ('Vladimir', 'Trajkov', 'vladimir.trajkov@finki.ukim.mk', '$2b$12$6GpYjsM8PpXVpB7m0JPwiOkNZik4NVnqz1cKae5HQG6Wv2mIw2fMS', 'PROFESSOR');
+
+
+INSERT INTO students (user_id) VALUES
+                                   (1),
+                                   (2),
+                                   (3),
+                                   (4),
+                                   (5),
+                                   (6),
+                                   (7),
+                                   (8),
+                                   (9),
+                                   (10),
+                                   (11),
+                                   (12),
+                                   (13),
+                                   (14),
+                                   (15),
+                                   (16),
+                                   (17),
+                                   (18),
+                                   (19);
+
+INSERT INTO teachers (user_id, employment_status) VALUES
+                                                      (20,0),
+                                                      (21,0),
+                                                      (22,0);
+
+INSERT INTO public.subjects (name, description, difficulty_level, created_at, teacher_id) VALUES
+                                                                                              ('Database Systems', 'Introduction to relational databases, SQL, and database design.', 3, now(), 1),
+                                                                                              ('Operating Systems', 'Concepts of process management, memory management, and file systems.', 3, now(), 2),
+                                                                                              ('Artificial Intelligence', 'Basics of AI, machine learning, and neural networks.', 3, now(), 3);
+
+
+INSERT INTO public.student_subject_access (student_id, subject_id, access_granted_at)
+VALUES  (1, 1, now()),
+        (2, 1, now()),
+        (3, 1, now()),
+        (4, 1, now()),
+        (5, 1, now()),
+        (6, 1, now()),
+        (7, 1, now()),
+        (8, 1, now()),
+        (9, 1, now()),
+        (10, 1, now()),
+        (11, 1, now()),
+        (12, 1, now()),
+        (13, 1, now()),
+        (14, 1, now()),
+        (15, 1, now()),
+        (16, 1, now()),
+        (17, 1, now()),
+        (18, 1, now()),
+        (19, 1, now());
+
+INSERT INTO public.student_subject_access (student_id, subject_id, access_granted_at)
+VALUES  (1, 2, now()),
+        (2, 2, now()),
+        (3, 2, now()),
+        (4, 2, now()),
+        (5, 2, now()),
+        (6, 2, now()),
+        (7, 2, now()),
+        (8, 2, now()),
+        (9, 2, now()),
+        (10, 2, now()),
+        (11, 2, now()),
+        (12, 2, now()),
+        (13, 2, now()),
+        (14, 2, now()),
+        (15, 2, now()),
+        (16, 2, now()),
+        (17, 2, now()),
+        (18, 2, now()),
+        (19, 2, now());
+
+
+INSERT INTO public.student_subject_access (student_id, subject_id, access_granted_at)
+VALUES  (1, 3, now()),
+        (2, 3, now()),
+        (3, 3, now()),
+        (4, 3, now()),
+        (5, 3, now()),
+        (6, 3, now()),
+        (7, 3, now()),
+        (8, 3, now()),
+        (9, 3, now()),
+        (10, 3, now()),
+        (11, 3, now()),
+        (12, 3, now()),
+        (13, 3, now()),
+        (14, 3, now()),
+        (15, 3, now()),
+        (16, 3, now()),
+        (17, 3, now()),
+        (18, 3, now()),
+        (19, 3, now());
