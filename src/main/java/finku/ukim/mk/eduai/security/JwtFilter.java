3e52dd593de9e27421fc63d816f3f7bb0225003c
vercchy <verchepetrushevska2@gmail.com>
2025-07-16 23:51:53 +0200

    Add missed logger in JWTFilter due to Tomcat errors

--- src/main/java/finku/ukim/mk/eduai/security/JwtFilter.java
+++ src/main/java/finku/ukim/mk/eduai/security/JwtFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +17,8 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+
+    private static final Log logger = LogFactory.getLog(JwtFilter.class);
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
