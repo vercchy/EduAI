b004a6764e4d9c76b0e810eef2a88e3ef1440f2b
vercchy <verchepetrushevska2@gmail.com>
2025-07-16 23:11:06 +0200

    Update config file to manually create entity manager factory

--- src/main/java/finku/ukim/mk/eduai/config/DataSourceConfig.java
+++ src/main/java/finku/ukim/mk/eduai/config/DataSourceConfig.java
@@ -1,15 +1,23 @@
 package finku.ukim.mk.eduai.config;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableTransactionManagement
 @Configuration
 @Profile("k8s")
 public class DataSourceConfig {
@@ -45,5 +53,27 @@ public class DataSourceConfig {
         routingDataSource.setDefaultTargetDataSource(writeDataSource);
         routingDataSource.setTargetDataSources(targetDataSources);
         return routingDataSource;
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource routingDataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(routingDataSource);
+        em.setPackagesToScan("finku.ukim.mk.eduai.model");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
     }
 }
